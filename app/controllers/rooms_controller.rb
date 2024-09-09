58037cca7422e6f334b2956e814809caba8d7937
Sasagamo <naoyasakamoto59@gmail.com>
2024-09-09 16:44:03 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,18 @@
 class RoomsController < ApplicationController
   def new
-    @room=Room.new
+    @room=Room.new()
   end
-
-  # private
-  # def room_params
-  #   params.require(:room).permit(:name)
-  # end
+  def create
+    @room =Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  def room_params
+    params.require(:room).permit(:name)
+  end
 end
